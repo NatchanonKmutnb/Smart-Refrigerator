65524450e8726d9980dcf37d6c312182269548e5
NatchanonKmutnb <pompkins@Pompkinss-Laptop.local>
2024-04-18 23:27:10 +0700

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Box, Drawer, Grid, IconButton } from "@mui/material";
+import { Cube } from "./components/Cube";
+import SideBar from "./layouts/Sidebar";
+import MySidebar from "./layouts/Sidebar";
+import { MenuOutlined } from "@mui/icons-material";
+import { Route, Routes, Redirect } from "react-router-dom";
+import Main from "./pages/Main";
+import RefrigeratorPage from "./pages/RefrigeratorPage";
 
-function App() {
+const boxes = [
+  { id: "box1", status: "empty" },
+  { id: "box2", status: "empty" },
+  { id: "box3", status: "full" },
+  { id: "box4", status: "empty" },
+  { id: "box5", status: "empty" },
+  { id: "box6", status: "full" },
+];
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [image, setImage] = useState(false);
+  const [toggled, setToggled] = useState(false);
+
+  const handleCollapsedChange = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const handleToggleSidebar = (value) => {
+    setToggled(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        position: "relative",
+        height: "100vh",
+        minHeight: "100vh",
+      }}
+    >
+      <MySidebar
+        collapsed={collapsed}
+        toggled={toggled}
+        handleToggleSidebar={handleToggleSidebar}
+        handleCollapsedChange={handleCollapsedChange}
+      />
+      <main style={{ flexGrow: 1 }}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/refrigerator/:id" element={<RefrigeratorPage />} />
+        </Routes>
+      </main>
+    </Box>
   );
-}
+};
 
 export default App;
---
+++ src/layouts/Sidebar.jsx
@@ -0,0 +1,77 @@
+import {
+  KeyboardArrowLeft,
+  KeyboardArrowRight,
+  Kitchen,
+} from "@mui/icons-material";
+import React from "react";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+} from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { useNavigate } from "react-router-dom";
+
+const MySidebar = ({
+  collapsed,
+  toggled,
+  handleToggleSidebar,
+  handleCollapsedChange,
+}) => {
+  const navigator = useNavigate();
+  return (
+    <ProSidebar
+      collapsed={collapsed}
+      toggled={toggled}
+      onToggle={handleToggleSidebar}
+      // breakPoint="sm"
+      // style={{ height: "100vh", position: "fixed", top: 0, left: 0 }}
+    >
+      {/* Header */}
+      <SidebarHeader>
+        <Menu iconShape="circle">
+          {collapsed ? (
+            <MenuItem
+              icon={<KeyboardArrowRight />}
+              onClick={handleCollapsedChange}
+            ></MenuItem>
+          ) : (
+            <MenuItem
+              suffix={<KeyboardArrowLeft />}
+              onClick={handleCollapsedChange}
+            >
+              <div
+                style={{
+                  padding: "9px",
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                  fontSize: 13,
+                  letterSpacing: "1px",
+                }}
+              >
+                Smart Refrigerator
+              </div>
+            </MenuItem>
+          )}
+        </Menu>
+      </SidebarHeader>
+      {/* Content */}
+      <SidebarContent>
+        <Menu iconShape="circle">
+          <SubMenu title={"Refrigerator"} icon={<Kitchen />}>
+            <MenuItem onClick={() => navigator("/refrigerator/1")}>
+              Number 1
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>About</MenuItem>
+        </Menu>
+      </SidebarContent>
+    </ProSidebar>
+  );
+};
+
+export default MySidebar;
